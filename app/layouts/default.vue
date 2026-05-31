3733e74f920c767ce1c623f51b1a4097ed6ce80c
Xifré Font <xifre.font.97@gmail.com>
2026-05-31 10:55:53 +0200

    First commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { t, locale } = useI18n();
+
+useHead({
+    meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+    htmlAttrs: { lang: locale },
+});
+
+useSeoMeta({
+    title: t("meta.title"),
+    description: t("meta.description"),
+    ogTitle: t("meta.title"),
+    ogDescription: t("meta.description"),
+});
+</script>
+
+<template>
+    <slot />
+</template>
